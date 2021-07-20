ee62619b26a9edd08268f2d73b1240416e75af33
Kars Frijters <kfrijters@shiftbase.com>
2021-07-20 17:40:47 +0200

    fix: Rector phpunit 4

--- rector.php
+++ rector.php
@@ -1,7 +1,13 @@
 <?php
+
+use Rector\Core\Configuration\Option;
 use Rector\PHPUnit\Set\PHPUnitSetList;
 use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
 
 return static function (ContainerConfigurator $containerConfigurator): void {
+    // paths to refactor; solid alternative to CLI arguments
+    $parameters = $containerConfigurator->parameters();
+    $parameters->set(Option::PATHS, [__DIR__ . '/app', __DIR__ . '/lib']);
+
     $containerConfigurator->import(PHPUnitSetList::PHPUNIT_40);
 };
